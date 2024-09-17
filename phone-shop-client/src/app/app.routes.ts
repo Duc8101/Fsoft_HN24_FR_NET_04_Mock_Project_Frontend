20647a9112ee09bf6a539f0d09009a7b3f09fe54
Duoc Nguyen <duocng.2409@gmail.com>
2024-09-17 16:56:36 +0700

    fix structure

--- phone-shop-client/src/app/app.routes.ts
+++ phone-shop-client/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes : Routes = [
   {path : 'login', component : LoginComponent},
