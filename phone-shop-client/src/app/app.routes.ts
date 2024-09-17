4719f5ad59d52e0ed7edd4e78c05b0a9edbf4b02
Duoc Nguyen <duocng.2409@gmail.com>
2024-09-17 16:47:34 +0700

    Revert "commit"
    This reverts commit ee4b118e29cb77f1fa239aba272aa107a3d75c51.

--- phone-shop-client/src/app/app.routes.ts
+++ phone-shop-client/src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-<<<<<<< Updated upstream
-=======
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
->>>>>>> Stashed changes
 
 export const routes: Routes = [];
