65616cc8b1d6a7700c6405605699713839b42776
Duoc Nguyen <duocng.2409@gmail.com>
2024-09-19 11:35:07 +0700

    all-product screen

--- phone-shop-client/src/app/app.routes.ts
+++ phone-shop-client/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { SearchComponent } from './components/search/search.component';
+import { AllProductComponent } from './components/all-product/all-product.component';
 
 export const routes : Routes = [
   {path : 'login', component : LoginComponent},
   {path : 'register', component : RegisterComponent},
   {path : 'forgot-password', component : ForgotPasswordComponent},
   {path : 'search', component : SearchComponent},
+  {path : 'all-product', component : AllProductComponent},
 ];
